365af7bbee34e5a77dae7a64f3a452756bcf5517
Крушельницкий Андрей Вячеславович <akrushelnitskiy@at-consulting.ru>
2014-09-03 12:19:32 +0400

    first commit

--- cli.ps1
+++ cli.ps1
@@ -1,5 +1,5 @@
-
+# All Datastore info
 Get-Datacenter SAV,HUTOR | ForEach-Object {
 	$DC = $_
 	Get-Datacenter $DC | Get-Datastore | Select @{N="Datacenter";E={@($DC)}}, Name, @{N="TotalSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity)/1GB,0)}}, @{N="Type";E={@($_.Type)}}, @{N="UsedSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity - $_.ExtensionData.Summary.FreeSpace)/1GB,0)}}, @{N="ProvisionedSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity - $_.ExtensionData.Summary.FreeSpace + $_.ExtensionData.Summary.Uncommitted)/1GB,0)}}, @{N="FreeSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.FreeSpace)/1GB,0)}}, @{N="NumVM";E={@($_ | Get-VM ).Count}},@{N="NumVMHost";E={@($_ | Get-VMHost ).Count}} 
-} | Export-Csv -Path C:\vmware\vmds-datastore.csv -encoding unicode -NoTypeInformation -UseCulture
+} | Export-Csv -Path C:\vmware\stat\datastores.csv -NoTypeInformation -UseCulture
