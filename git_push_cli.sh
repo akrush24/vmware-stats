a205e8c75f3b95fea9a8c391c2fa1a19fc380cef
Крушельницкий Андрей Вячеславович <akrushelnitskiy@at-consulting.ru>
2014-09-10 14:24:25 +0400

    Wed Sep 10 14:24:25     2014, from Windows

--- git_push_cli.sh
+++ git_push_cli.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd c:/script/stat/
 git add -A 
 git commit -m "`date`, from Windows"
 git push -u origin master
--- start.bat
+++ start.bat
@@ -1,1 +1,2 @@
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -psc "C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\vim.psc1" -c ". \"c:\script\stat\cli.ps1\""
+c:\script\stat\git_push_cli.sh
