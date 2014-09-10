2d8557c93ce29d7e3765ccaf5665fa1acbf3e1a2
Крушельницкий Андрей Вячеславович <akrushelnitskiy@at-consulting.ru>
2014-09-10 14:19:46 +0400

    Wed Sep 10 14:19:46     2014, from Windows

--- cli.ps1
+++ cli.ps1
@@ -1,5 +1,6 @@
 # All Datastore info
+Connect-VIServer vcenter.at-consulting.ru
 Get-Datacenter SAV,HUTOR | ForEach-Object {
 	$DC = $_
 	Get-Datacenter $DC | Get-Datastore | Select @{N="Datacenter";E={@($DC)}}, Name, @{N="TotalSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity)/1GB,0)}}, @{N="Type";E={@($_.Type)}}, @{N="UsedSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity - $_.ExtensionData.Summary.FreeSpace)/1GB,0)}}, @{N="ProvisionedSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity - $_.ExtensionData.Summary.FreeSpace + $_.ExtensionData.Summary.Uncommitted)/1GB,0)}}, @{N="FreeSpaceGB";E={[Math]::Round(($_.ExtensionData.Summary.FreeSpace)/1GB,0)}}, @{N="NumVM";E={@($_ | Get-VM ).Count}},@{N="NumVMHost";E={@($_ | Get-VMHost ).Count}} 
-} | Sort Name | Export-Csv -Path C:\vmware\stat\datastores.csv -NoTypeInformation -UseCulture
+} | Sort Name | Export-Csv -Path C:\script\stat\datastores.csv -NoTypeInformation -UseCulture
